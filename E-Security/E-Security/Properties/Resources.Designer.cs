2173f8ec51bd357b5eeb3d9b8974271bffd014d7
Lanfor Mena Montalbán <lan4mena@gmail.com>
2018-06-08 10:01:08 -0600

    08/06/2018 10:00 a.m.

--- E-Security/E-Security/Properties/Resources.Designer.cs
+++ E-Security/E-Security/Properties/Resources.Designer.cs
@@ -109,5 +109,15 @@ namespace E_Security.Properties {
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap mesnos {
+            get {
+                object obj = ResourceManager.GetObject("mesnos", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }
